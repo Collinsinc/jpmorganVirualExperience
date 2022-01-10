c25d01d099ec8baf3dd8ddf4afcc632360409f9c
Simangaliso Collin Mavuso <80270764+Collinsinc@users.noreply.github.com>
2022-01-10 17:36:54 +0300

    Add files via upload

---
+++ JPMC-tech-task-1-py3/client_test.py
@@ -0,0 +1,46 @@
+import unittest
+from client3 import getDataPoint, getRatio
+
+class ClientTest(unittest.TestCase):
+  def test_getDataPoint_calculatePrice(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
+
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
+
+  """ ------------ Add more unit tests ------------ """
+
+  def test_getRatio_divisionByZero(self):
+
+   self.assertEqual(getRatio(5,0),0)
+
+  def test_getRatio_priceAisZero(self):
+
+    self.assertEqual(getRatio(0,67.5),0)
+    
+  def test_getRatio_GreaterThan1(self):
+
+   self.assertGreater(getRatio(56.5,34.5),1)
+
+  def test_getRatio_LessThan1(self):
+
+    self.assertLess(getRatio(129.5,553.6),1)
+  
+  def test_getRatio_Equals1(self):
+
+    self.assertEqual(getRatio(1,1),1)
+
+if __name__ == '__main__':
+    unittest.main()
